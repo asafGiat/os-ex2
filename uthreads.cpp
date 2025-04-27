be5f7d39801370dfb6ce3feb4786c82a7b8fcef2
Mic-Toc <10michaelmar@gmail.com>
2025-04-27 20:33:29 +0300

    creating queue and printing a null error

--- uthreads.cpp
+++ uthreads.cpp
@@ -17,8 +17,20 @@ typedef struct thread
     state state;
 }thread;
 
+std::queue<thread> threadQ;
+
 
 int uthread_init(int quantum_usecs)
 {
     return 0;
+}
+
+int uthread_spawn(thread_entry_point entry_point)
+{
+    if (entry_point == nullptr)
+    {
+        fprintf(stderr, "thread library error: The entry point should not be null.\n");
+    }
+    thread thread;
+
 }
